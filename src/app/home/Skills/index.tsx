8bbe73716145c5b6dc6b7d4de46171c4af10ec72
hurkatsparsh <hurkatsparsh@gmail.com>
2024-07-01 23:19:27 +0530

    removed html in skills

--- src/app/home/Skills/index.tsx
+++ src/app/home/Skills/index.tsx
@@ -6,6 +6,7 @@ import styles from "./styles";
 import useStyles from "@/app/hooks/useStyles";
 import BarChart from "@/app/components/barChart/barChart";
 import { bars, levels } from "@/app/models/commonModel";
+import CustomTypography from "@/app/components/customTypography";
 
 const SkillsContainer = () => {
   const classes = useStyles(styles);
@@ -26,29 +27,7 @@ const SkillsContainer = () => {
         <Grid item>
           <Grid sx={classes.skillsContent} container>
             <Grid xs item>
-              <Typography>
-                I am a web developer with a solid <b>two years</b> of experience
-                under my belt as of 2024.
-                <br />
-                <b>Javascript</b> is my playground, and I'm always diving deeper
-                into its intricacies to push the boundaries of what's possible.
-                Crafting dynamic websites is my forte, especially with{" "}
-                <b>ReactJS</b>. This website marks my first real world
-                application experience with <b>NextJS</b> (let me know how i did
-                and how i can improve). Additionally I have experience with{" "}
-                <b>NodeJS</b> for server-side networking.
-                <br />
-                Back in my college days, I aced <b>Java and Python</b>, but my
-                heart truly found its rhythm with JS post-graduation. While I
-                might have drifted from Java and Python amidst the captivating
-                world of JavaScript, my foundational skills in these languages
-                still stand strong.
-                <br />
-                <b>HTML/CSS</b>? Oh, you bet I've got those down pat. After all,
-                what kind of web developer doesn't rock those fundamentals? But
-                it's the art of combining these languages seamlessly that truly
-                brings my projects to life.
-              </Typography>
+              <CustomTypography text="I am a web developer with a solid **two years** of experience under my belt as of 2024. **Javascript** is my playground, and I'm always diving deeper into its intricacies to push the boundaries of what's possible. Crafting dynamic websites is my forte, especially with **ReactJS**. This website marks my first real world application experience with **NextJS** (let me know how i did and how i can improve). Additionally I have experience with **NodeJS** for server-side networking. Back in my college days, I aced **Java and Python**, but my heart truly found its rhythm with JS post-graduation. While I might have drifted from Java and Python amidst the captivating world of JavaScript, my foundational skills in these languages still stand strong. **HTML/CSS**? Oh, you bet I've got those down pat. After all, what kind of web developer doesn't rock those fundamentals? But it's the art of combining these languages seamlessly that truly brings my projects to life." />
             </Grid>
 
             <Grid xs item>
