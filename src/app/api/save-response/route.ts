7a0a8e7b2605767e84be1e60b2fd4b13c8a13dc0
hurkatsparsh <hurkatsparsh@gmail.com>
2024-07-02 00:55:44 +0530

    error handling in save response api

--- src/app/api/save-response/route.ts
+++ src/app/api/save-response/route.ts
@@ -1,12 +1,6 @@
 import { connectToDB } from "@/app/db/dbConfig";
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request) {
-  return NextResponse.json({ a: "Hey" });
-}
-
-export async function HEAD(request: Request) {}
-
 export async function POST(request: Request) {
   const conn = connectToDB();
   let body = await request.json();
@@ -15,27 +9,28 @@ export async function POST(request: Request) {
     timeZone: "Asia/Kolkata",
   });
   const istDate = new Date(istString);
-  conn.query(
-    "INSERT INTO spot (`messages`, `responses`, `date`) VALUES (?, ?, ?);",
-    [query, response.content, istDate],
-    (err, ok) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(ok.insertId);
-      }
-    }
-  );
-  conn.end();
-
-  return NextResponse.json({ status: "SUCCESS" });
+  try {
+    const rows = await new Promise<any>((resolve, reject) => {
+      conn.query(
+        "INSERT INTO spot (`messages`, `responses`, `date`) VALUES (?, ?, ?);",
+        [query, response.content, istDate],
+        (err, ok) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(ok.insertId);
+          }
+        }
+      );
+    });
+    conn.end();
+    return NextResponse.json({ data: rows, status: "SUCCESS" });
+  } catch (error) {
+    conn.end();
+    return NextResponse.json({
+      data: [],
+      error,
+      status: "ERROR",
+    });
+  }
 }
-
-export async function PUT(request: Request) {}
-
-export async function DELETE(request: Request) {}
-
-export async function PATCH(request: Request) {}
-
-// If `OPTIONS` is not defined, Next.js will automatically implement `OPTIONS` and  set the appropriate Response `Allow` header depending on the other methods defined in the route handler.
-export async function OPTIONS(request: Request) {}
