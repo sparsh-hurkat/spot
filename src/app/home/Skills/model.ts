eac9f57cf8e807d88dfa145def3122d79722131b
sparsh-hurkat <hurkatsparsh@gmail.com>
2025-12-03 23:35:15 -0500

    update skills

--- src/app/home/Skills/model.ts
+++ src/app/home/Skills/model.ts
@@ -34,9 +34,11 @@ export const bars = [
 
 export const skillsLang = {
   skillsContent1: `I am a full-stack developer with a solid **three years** of experience under my belt as of 2025. I got my start in frontend web development, and quickly expanded my expertise to full-stack applications.
-**Javascript** is my playground, and I'm always diving deeper into its intricacies to push the boundaries of what's possible. Crafting dynamic websites is my forte, especially with **React.js**.
-**HTML/CSS**? Oh, you bet I've got those down pat. After all, what kind of web developer doesn't rock those fundamentals? But it's the art of combining these languages seamlessly that truly brings my projects to life.`,
+**Javascript** is my playground, and I'm always diving deeper into its intricacies to push the boundaries of what's possible. Crafting dynamic websites is my forte, especially with **React.js** and **TypeScript**.
+**HTML/CSS**? Oh, you bet I've got those down pat. After all, what kind of web developer doesn't rock those fundamentals? But it's the art of combining these languages seamlessly that truly brings my projects to life. I’ve also built reusable UI systems and component libraries, and even experimented with interactive mobile experiences using **React Native**.`,
+
   skillsContent2: `On the other hand, I use **Spring Boot** to build scalable backend services, **Node.js** for server-side applications, and **SST.dev** for deploying serverless cloud-based solutions on **AWS**. Additionally, I have worked with **MySQL** and **MongoDB** for database management.
-Back in my college days, I aced **Java and Python**, but my heart truly found its rhythm with JS post-graduation. While I might have drifted from Java and Python amidst the captivating world of JavaScript, my foundational skills in these languages still stand strong.
-This website marks my first real world application experience with **Next.js** (let me know how i did and how i can improve). I even utilized **Langchain** to showcase my capabilities in developing a cheap and efficient **LLM wrapper**.`,
+Back in my college days, I aced **Python**, but my heart truly found its rhythm with Java & JS post-graduation. While I might have drifted from Python amidst the captivating world of Java & JavaScript, my foundational skills in these languages still stand strong.
+This website marks my first real world application experience with **Next.js** (let me know how i did and how i can improve). I even utilized **Langchain** to showcase my capabilities in developing a cheap and efficient **LLM wrapper**. I also apply **Jest** for unit testing and follow modern engineering workflows with **CI/CD** and Agile practices.`,
 };
+
